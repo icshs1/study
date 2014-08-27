3db20290867134009b1b613948b1b283cd218450
Hyunsik Shim <icshs1@naver.com>
2014-08-27 21:01:26 +0900

    add crawling data

--- Study_20140826.R
+++ Study_20140826.R
@@ -30,11 +30,6 @@ GetDaumMovieData <- function(num){
 }
 
 
-
-
-
-
-
 #네이트 영화 페이지에서 감상평 내용 수집하는 모듈
 GetNateMovieData <- function(num){
   url = gsub(" ","",paste("http://movie.nate.com/comm/reply40/?cinema_id=&type=&tab=all&page=",num))  
@@ -65,29 +60,29 @@ GetNateMovieData <- function(num){
   return(senti_data)
 }
 
+Crawling_MovieData <-function(startpage,endpage) {
+  #실제 추출하는 부분 (페이지별로 추출함수 호출)
+  DATA<-NULL        #감상평 데이터 변수 초기화
+  StartPage <- startpage    #시작페이지
 
+  #크롤링에 오랜시간이 걸리므로 여기에서는 10페이지만 샘플로 수집하기로 한다.
+  EndPage <- endpage     #마지막페이지 
+  #추출하고자 하는 사이트의 함수를 호출해서 데이터를 수집한다.
+  cdir<-getwd() ##  current directory
+  directory="reviewdata"
 
-#실제 추출하는 부분 (페이지별로 추출함수 호출)
-DATA<-NULL        #감상평 데이터 변수 초기화
-StartPage <- 1    #시작페이지
-
-#크롤링에 오랜시간이 걸리므로 여기에서는 10페이지만 샘플로 수집하기로 한다.
-EndPage <- 10     #마지막페이지 
-
-
-#추출하고자 하는 사이트의 함수를 호출해서 데이터를 수집한다.
-#다음만 수집할 것이므로 네이{
-getData<-GetDaumMovieData(i)  #다음 크롤링 
-#getData<-GetNateMovieData(i)  #네이트 크롤링 
-DATA<-rbind(DATA,getData) #데이터 바인드
+  for (i in StartPage:EndPage) {
+    getData<-GetDaumMovieData(i)  ##다음 크롤링 
+    DATA<-rbind(DATA,getData) #데이터 바인드
+  }
+  #수집된 감상평 데이터를 CSV파일로 저장한다.
+  #아래의 파일은 제일 상단메뉴의 Tools -> Options..에서
+  #Default working directory에 지정된 폴더에 
+  #SAMPLE_MOVIE_DATA.csv 이름으로 생성된다.
+  filename<-paste("SAMPLE_MOVIE_DATA_DAUM_",as.character(StartPage),"_",as.character(EndPage),".csv",sep="")
+  write_filename<-file.path(cdir,directory,filename,fsep="\\")
+  write.csv(DATA,write_filename)
 }
-
-
-#수집된 감상평 데이터를 CSV파일로 저장한다.
-#아래의 파일은 제일 상단메뉴의 Tools -> Options..에서
-#Default working directory에 지정된 폴더에 
-#SAMPLE_MOVIE_DATA.csv 이름으로 생성된다.
-write.csv(DATA,"SAMPLE_MOVIE_DATA.csv")
 
 
 
